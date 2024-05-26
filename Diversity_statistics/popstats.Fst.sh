f9f7ba618950fe34858da4a64996c4e8e654fd28
karine Durand <45596232+karinedurand@users.noreply.github.com>
2024-05-26 17:42:06 +0200

    Update popstats.Fst.sh

--- Diversity_statistics/popstats.Fst.sh
+++ Diversity_statistics/popstats.Fst.sh
@@ -8,8 +8,8 @@ cd /lustre/durandk/slitto_newref/VariantCalling/diversity
 
 
  
-/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --weir-fst-pop SA.pop  --weir-fst-pop EGT.pop --fst-window-size  500000 --fst-window-step  50000  --out SA_EGT  
+/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --weir-fst-pop SA.pop    --weir-fst-pop EGT.pop --fst-window-size  500000 --fst-window-step  50000  --out SA_EGT  
  
-/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --weir-fst-pop ML.pop  --weir-fst-pop EGT.pop --fst-window-size  500000 --fst-window-step  50000  --out ML_EGT  
+/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --weir-fst-pop ML.pop    --weir-fst-pop EGT.pop --fst-window-size  500000 --fst-window-step  50000  --out ML_EGT  
 
 /storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --weir-fst-pop MLSA.pop  --weir-fst-pop EGT.pop --fst-window-size  500000 --fst-window-step  50000  --out MLSA_EGT  
