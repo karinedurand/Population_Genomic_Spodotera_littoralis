56c2e133031a061dff72c6e25ae7576c39fa3358
karine Durand <45596232+karinedurand@users.noreply.github.com>
2024-05-26 17:40:56 +0200

    Update popstats.pi.sh

--- Diversity_statistics/popstats.pi.sh
+++ Diversity_statistics/popstats.pi.sh
@@ -6,9 +6,9 @@ module load cv-standard
 cd /lustre/durandk/slitto_newref/VariantCalling/diversity
 
  
-/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --window-pi 500000 --out SA_pi --keep SA.pop
+/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --window-pi 100000 --out SA_pi --keep SA.pop
  
-/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --window-pi 500000 --out ML_pi --keep ML.pop
+/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --window-pi 100000 --out ML_pi --keep ML.pop
 
-/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --window-pi 500000 --out EGT_pi --keep EGT.pop
+/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --window-pi 100000 --out EGT_pi --keep EGT.pop
 
