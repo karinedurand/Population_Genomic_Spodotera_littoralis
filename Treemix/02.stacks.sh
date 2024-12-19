dcb44fe4432aa137c68f950afccac764d9c98861
karine Durand <45596232+karinedurand@users.noreply.github.com>
2024-12-19 11:21:56 +0100

    Update 02.stacks.sh

--- Treemix/02.stacks.sh
+++ Treemix/02.stacks.sh
@@ -2,11 +2,6 @@
 #SBATCH -p dgimi-eha
 #SBATCH --mem=50G
 
-#Load modules
 
 
-
-
-#vcftools --gzvcf /work/kdurand/dgimi/sfrugi_hybrid/VCF/Invasive_117.vcf.gz --max-missing 1 --thin 100 --recode --stdout | gzip > Invasive_117.noN.thin100.vcf.gz
-
-populations -V slittonewref_2023_outgroup.SNP.TREEMIX.vcf.gz  -M 6pop.txt -t 8 --out-path treemix_6pops --treemix
+/storage/simple/projects/faw_adaptation/programs/stacks-2.68/populations -V slittonewref_2023_outgroup.SNP.TREEMIX.vcf.gz  -M 6pop.txt -t 8 --out-path treemix_6pops --treemix
