4c74b3ceb38e019030cdcea5c3742a9fa3019830
karine Durand <45596232+karinedurand@users.noreply.github.com>
2026-09-10 14:11:19 +0200

    Update popstats.Fst.sh

--- Diversity_statistics/popstats.Fst.sh
+++ Diversity_statistics/popstats.Fst.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-#SBATCH --partition=dgimi-eha
-
-module load cv-standard
-
-cd /lustre/durandk/slitto_newref/VariantCalling/diversity
+#SBATCH -p cpu-dedicated
+#SBATCH --account=dedicated-cpu@dgimi-eha
+#SBATCH --cpus-per-task=16
 
 
+VCF="/storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_GenFAW600/2025_GenFAW600/VCF_notpruned/GenFAW_max5miss_biallelic.vcf.gz"
+module load bioinfo-ifb
+module load  vcftools/0.1.16  
+OUTDIR="fst_observed"
+mkdir -p $OUTDIR
 
- 
-/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --weir-fst-pop SA.pop    --weir-fst-pop EGT.pop --fst-window-size  500000 --fst-window-step  50000  --out SA_EGT  
- 
-/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --weir-fst-pop ML.pop    --weir-fst-pop EGT.pop --fst-window-size  500000 --fst-window-step  50000  --out ML_EGT  
+vcftools  --gzvcf $VCF --weir-fst-pop Nat_C.pop --weir-fst-pop Nat_R.pop \
+    --fst-window-size 100000 --fst-window-step 10000 --out $OUTDIR/Nat_C_vs_Nat_R
 
-/storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools --gzvcf /lustre/durandk/slitto_newref/VariantCalling/VCF/slittonewref_2023.SNP.filtered.recode.vcf.gz --weir-fst-pop MLSA.pop  --weir-fst-pop EGT.pop --fst-window-size  500000 --fst-window-step  50000  --out MLSA_EGT  
+vcftools  --gzvcf $VCF --weir-fst-pop Inv_east.pop --weir-fst-pop Inv_west.pop \
+    --fst-window-size 100000 --fst-window-step 10000 --out $OUTDIR/Inv_east_vs_Inv_west
+
+vcftools  --gzvcf $VCF --weir-fst-pop Nat_C_Nat_R_Mex.pop --weir-fst-pop Nat_C_Nat_R.pop \
+    --fst-window-size 100000 --fst-window-step 10000 --out $OUTDIR/NatCNatRMex_vs_NatCNatR
+
+vcftools  --gzvcf $VCF --weir-fst-pop Zam.pop --weir-fst-pop Inv_east_west.pop \
+    --fst-window-size 100000 --fst-window-step 10000 --out $OUTDIR/Zam_vs_InvEastWest
+
+vcftools  --gzvcf $VCF --weir-fst-pop Nat_C_Nat_R_Mex.pop --weir-fst-pop Inv_east_west_zam.pop \
+    --fst-window-size 100000 --fst-window-step 10000 --out $OUTDIR/NatCNatRMex_vs_InvEastWestZam
